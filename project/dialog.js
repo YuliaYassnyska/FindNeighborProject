ddadd4f6924382d603938a88329e1055f1c2c6de
unknown <you@example.com>
2019-12-08 13:10:12 +0200

    first demo of site

---
+++ project/dialog.js
@@ -0,0 +1,7 @@
+var dialog = document.querySelector('dialog');
+document.querySelector('#openDialog').onclick = function() {
+    dialog.show();
+}
+document.querySelector('#closeDialog').onclick = function() {
+    dialog.close();
+}
